7a498e1292372af03352053bd01940517ab61b21
Tianxue30 <2980814653@qq.com>
2026-08-09 00:28:32 +0800

    deploy: sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+// 极简离线缓存：让工作台在手机"添加到主屏幕"后能离线打开
+const CACHE = "spine-diary-v1";
+const ASSETS = [".", "index.html", "manifest.webmanifest", "icon.svg"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", (e) => {
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request).then((resp) => {
+      const copy = resp.clone();
+      caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(() => {});
+      return resp;
+    }).catch(() => caches.match("index.html")))
+  );
+});
