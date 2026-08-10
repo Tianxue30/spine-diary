1ba9d5a82f7092f89c803be30873faeb42d8185d
Tianxue30 <2980814653@qq.com>
2026-08-10 17:05:23 +0800

    test ref=main

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // 离线缓存：让工作台在手机"添加到主屏幕"后能离线打开
 // 导航/index.html 采用「网络优先」，确保重新部署后用户能立即拿到最新页面；
 // 其余静态资源仍缓存优先，保证离线可用。
-const CACHE = "spine-diary-v2";
-const ASSETS = [".", "index.html", "manifest.webmanifest", "icon.svg"];
+const CACHE = "spine-diary-v3";
+const ASSETS = [".", "index.html", "manifest.webmanifest", "icon.svg", "icon-192.png", "icon-512.png"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
